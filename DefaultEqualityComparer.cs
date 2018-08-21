cb73717c857fe69925f2f5aacf678dd4289a6520
huanent <huanent@hotmail.com>
2018-08-21 17:23:56 +0800

    init

---
+++ DefaultEqualityComparer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace System.Collections.Generic
+{
+    public class DefaultEqualityComparer<T> : EqualityComparer<T>
+    {
+        readonly Func<T, T, bool> _comparer;
+        readonly Func<T, int> _hash;
+
+        public DefaultEqualityComparer(Func<T, T, bool> comparer)
+        {
+            _comparer = comparer;
+        }
+
+        public DefaultEqualityComparer(Func<T, T, bool> comparer, Func<T, int> hash)
+        {
+            _comparer = comparer;
+            _hash = hash;
+        }
+        public override bool Equals(T x, T y)
+        {
+            return _comparer(x, y);
+        }
+
+        public override int GetHashCode(T obj)
+        {
+            return _hash(obj);
+        }
+    }
+}
